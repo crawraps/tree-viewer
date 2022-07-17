e2b4917a3ad04995229e9bec8138a813c62a8b94
crawraps <crawraps@gmail.com>
2022-07-17 03:26:48 +0300

    Update tree viewer component

--- src/components/TreeViewer.ts
+++ src/components/TreeViewer.ts
@@ -1,7 +1,23 @@
-export default class TreeViewer extends HTMLElement {
-  static DOMName = 'tree-viewer'
+import { TreeViewerData } from './../treeViewerData'
+import { CustomElement } from './CustomElement'
+
+export default class TreeViewer extends CustomElement {
+  static readonly DOMName = 'tree-viewer'
+  protected readonly state: TreeViewerData
 
   constructor() {
     super()
+
+    this.setStyle({
+      position: 'absolute',
+      top: '0px',
+      left: '0px',
+      minWidth: '100px',
+      width: '350px',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor: '#3d3d3d',
+    })
   }
 }
